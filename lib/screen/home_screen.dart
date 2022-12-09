3c1ef4d580c9bfd771a18412a7cf1428a92fc330
ilham <abc@ABCs-MacBook-Pro.local>
2022-12-09 11:11:56 +0700

    dashboard v1 done

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lottie/lottie.dart';
+import 'package:intl/intl.dart';
 
 import '../helper/apps_style.dart';
-import '../helper/size_config.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             //app bar
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -31,21 +32,18 @@ class _HomeScreenState extends State<HomeScreen> {
                       //name
                       Text('Selamat Datang',
                           style: kPoppinsRegularBold.copyWith(
-                              color: kGrey,
-                              fontSize: 20)),
+                              color: kGrey, fontSize: 20)),
                       const SizedBox(
                         height: 6,
                       ),
                       Text('Ilham Taufik',
-                          style: kPoppinsBold.copyWith(
-                              fontSize: 18)),
+                          style: kPoppinsBold.copyWith(fontSize: 18)),
                     ],
                   ),
 
                   //Profile Picture
                   InkWell(
-                    onTap: () async {
-                    },
+                    onTap: () async {},
                     child: Container(
                       padding: const EdgeInsets.all(12),
                       decoration: BoxDecoration(
@@ -84,7 +82,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     children: [
                       //card -> hhow do you feel?
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
                         child: Container(
                           padding: const EdgeInsets.all(20),
                           decoration: BoxDecoration(
@@ -94,12 +92,11 @@ class _HomeScreenState extends State<HomeScreen> {
                             children: [
                               //animation or picture
                               Container(
-                                padding: const EdgeInsets.all(12),
-                                height: 100,
-                                width: 100,
-                                child:
-                                    SvgPicture.asset("assets/icons/welcome.svg")
-                              ),
+                                  padding: const EdgeInsets.all(12),
+                                  height: 100,
+                                  width: 100,
+                                  child: SvgPicture.asset(
+                                      "assets/icons/welcome.svg")),
                               const SizedBox(
                                 width: 20,
                               ),
@@ -108,22 +105,18 @@ class _HomeScreenState extends State<HomeScreen> {
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text(
-                                      'Today',
-                                      style: kPoppinsBold.copyWith( fontSize: 18)
-                                    ),
+                                    Text(DateFormat('EEEE').format(date),
+                                        style: kPoppinsBold.copyWith(
+                                            fontSize: 18)),
                                     const SizedBox(
                                       height: 8,
                                     ),
-                                    Text(
-                                      '06 Desember 2022',
-                                      style: kPoppinsRegularBold.copyWith(
-                                            fontSize: 14)
-                                    ),
+                                    Text(DateFormat('d MMM, yyyy').format(date),
+                                        style: kPoppinsRegularBold.copyWith(
+                                            fontSize: 14)),
                                     const SizedBox(
                                       height: 12,
                                     ),
-                                    
                                   ],
                                 ),
                               )
@@ -136,23 +129,40 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
 
                       //search bar
-                      // Padding(
-                      //   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      //   child: Container(
-                      //     padding: const EdgeInsets.all(8),
-                      //     decoration: BoxDecoration(
-                      //       color: const Color.fromARGB(255, 203, 204, 235),
-                      //       borderRadius: BorderRadius.circular(12),
-                      //     ),
-                      //     child: const TextField(
-                      //       decoration: InputDecoration(
-                      //         prefixIcon: Icon(Icons.search),
-                      //         border: InputBorder.none,
-                      //         hintText: 'How can we help you?',
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 203, 204, 235),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("CSW Office",
+                                        style: kPoppinsMediumBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                    Text("08.00 - 08.30 WIB",
+                                        style: kPoppinsRegularBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                  ],
+                                ),
+                                Container(
+                                    padding: const EdgeInsets.all(12),
+                                    decoration: BoxDecoration(
+                                        color: kLightBlue,
+                                        borderRadius:
+                                            BorderRadius.circular(12)),
+                                    child: Text('Check In',
+                                        style: kPoppinsMediumBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14))),
+                              ],
+                            )),
+                      ),
 
                       const SizedBox(
                         height: 25,
@@ -160,31 +170,138 @@ class _HomeScreenState extends State<HomeScreen> {
 
                       //doctor list
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            const Text(
-                              'Berita\n terbaru',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                            InkWell(
-                              child: Text(
-                                'Lihat semua',
-                                style: TextStyle(
-                                  color: Colors.grey[600],
-                                  fontSize: 15,
-                                ),
-                              ),
-                            )
+                            Text('Menu lain',
+                                style: kPoppinsMediumBold.copyWith(
+                                    color: kDarkBlue, fontSize: 16)),
                           ],
                         ),
                       ),
                       const SizedBox(
                         height: 25,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: kWhite,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      color: Colors.cyan,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: const InkWell(
+                                      child: Icon(
+                                    Icons.format_quote_rounded,
+                                    color: Colors.white,
+                                  )),
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Request Lembur",
+                                        style: kPoppinsMediumBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                    Text("08.00 - 08.30 WIB",
+                                        style: kPoppinsRegularBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                  ],
+                                ),
+                              ],
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: kWhite,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      color: Colors.orange,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: const InkWell(
+                                      child: Icon(
+                                    Icons.payment_rounded,
+                                    color: Colors.white,
+                                  )),
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Slip Gaji",
+                                        style: kPoppinsMediumBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                    Text("08.00 - 08.30 WIB",
+                                        style: kPoppinsRegularBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                  ],
+                                ),
+                              ],
+                            )),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: kWhite,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                      color: Colors.blueAccent,
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: const InkWell(
+                                      child: Icon(
+                                    Icons.picture_as_pdf,
+                                    color: Colors.white,
+                                  )),
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("Surat-surat",
+                                        style: kPoppinsMediumBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                    Text("08.00 - 08.30 WIB",
+                                        style: kPoppinsRegularBold.copyWith(
+                                            color: kDarkBlue, fontSize: 14)),
+                                  ],
+                                ),
+                              ],
+                            )),
                       ),
                     ],
                   ),
