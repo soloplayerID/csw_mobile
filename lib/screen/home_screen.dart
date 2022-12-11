566cc2bcff4f3d616e76813135a8a8ebced1c55c
ilham <abc@ABCs-MacBook-Pro.local>
2022-12-11 14:08:05 +0700

    membuat date pick range

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:csw_attendance/screen/leave_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
@@ -266,41 +267,51 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                        child: Container(
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: kWhite,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Row(
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.all(12),
-                                  decoration: BoxDecoration(
-                                      color: Colors.blueAccent,
-                                      borderRadius: BorderRadius.circular(12)),
-                                  child: const InkWell(
-                                      child: Icon(
-                                    Icons.hotel_outlined,
-                                    color: Colors.white,
-                                  )),
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Izin",
-                                        style: kPoppinsMediumBold.copyWith(
-                                            color: kDarkBlue, fontSize: 14)),
-                                    Text("08.00 - 08.30 WIB",
-                                        style: kPoppinsRegularBold.copyWith(
-                                            color: kDarkBlue, fontSize: 14)),
-                                  ],
-                                ),
-                              ],
-                            )),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LeaveScreen()));
+                          },
+                          child: Container(
+                              padding: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                color: kWhite,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.all(12),
+                                    decoration: BoxDecoration(
+                                        color: Colors.blueAccent,
+                                        borderRadius:
+                                            BorderRadius.circular(12)),
+                                    child: const InkWell(
+                                        child: Icon(
+                                      Icons.hotel_outlined,
+                                      color: Colors.white,
+                                    )),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text("Izin",
+                                          style: kPoppinsMediumBold.copyWith(
+                                              color: kDarkBlue, fontSize: 14)),
+                                      Text("08.00 - 08.30 WIB",
+                                          style: kPoppinsRegularBold.copyWith(
+                                              color: kDarkBlue, fontSize: 14)),
+                                    ],
+                                  ),
+                                ],
+                              )),
+                        ),
                       ),
                       const SizedBox(
                         height: 12,
--- lib/screen/leave_screen.dart
+++ lib/screen/leave_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:csw_attendance/helper/apps_style.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:line_icons/line_icons.dart';
@@ -13,120 +14,256 @@ class LeaveScreen extends StatefulWidget {
 class _MyWidgetState extends State<LeaveScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: double.infinity,
-          child: Column(
-            children: [
-              Container(
-                color: const Color(0XFFA0D995),
-                padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-                height: 90,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.of(context).pop(true);
-                      },
-                      child: const Icon(LineIcons.arrowLeft,
-                          color: Colors.white, size: 30),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              color: kLightBlue,
+              padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop(true);
+                    },
+                    child: const Icon(LineIcons.arrowLeft,
+                        color: Colors.white, size: 30),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                          color: kLightWhite,
+                          borderRadius: BorderRadius.circular(12)),
+                      child: const InkWell(
+                          child: Icon(
+                        Icons.add,
+                        color: Colors.black,
+                      )),
                     ),
-                    // InkWell(
-                    //   onTap: () {
-                    //     Navigator.pushNamed(context, '/addKriteria');
-                    //   },
-                    //   child: const Icon(LineIcons.plus,
-                    //       color: Colors.white, size: 30),
-                    // ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: Container(
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              height: double.infinity,
+              color: const Color(0xffecedf2),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Leave',
+                              style: kPoppinsMediumBold.copyWith(
+                                  color: kDarkBlue, fontSize: 28)),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Text('December 2022',
+                        style: kPoppinsRegularBold.copyWith(
+                            color: kGrey, fontSize: 16)),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      height: 110,
+                      margin: const EdgeInsets.symmetric(vertical: 5.0),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Half Day Application',
+                                  style: kPoppinsRegularBold.copyWith(
+                                      color: kGrey, fontSize: 16)),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                      color: const Color(0xffFFF0C5),
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: Text('Awaiting',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kDarkBlue, fontSize: 14))),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Wed, 16 Dec',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kDarkBlue, fontSize: 14)),
+                                  Text('Berobat',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kLightBlue, fontSize: 14)),
+                                ],
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                    color: kLightWhite,
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: const InkWell(
+                                    child: Icon(
+                                  Icons.arrow_right,
+                                  color: kGrey,
+                                )),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 110,
+                      margin: const EdgeInsets.symmetric(vertical: 5.0),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('3 Day Application',
+                                  style: kPoppinsRegularBold.copyWith(
+                                      color: kGrey, fontSize: 16)),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                      color: const Color(0xffd0180e),
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: Text('Decline',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kLightWhite, fontSize: 14))),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Wed, 16 Dec',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kDarkBlue, fontSize: 14)),
+                                  Text('Jalan-jalan',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kLightBlue, fontSize: 14)),
+                                ],
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                    color: kLightWhite,
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: const InkWell(
+                                    child: Icon(
+                                  Icons.arrow_right,
+                                  color: kGrey,
+                                )),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 110,
+                      margin: const EdgeInsets.symmetric(vertical: 5.0),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Full Day Application',
+                                  style: kPoppinsRegularBold.copyWith(
+                                      color: kGrey, fontSize: 16)),
+                              Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                      color: const Color(0xffB4F5D0),
+                                      borderRadius: BorderRadius.circular(12)),
+                                  child: Text('Approved',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kDarkBlue, fontSize: 14))),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Wed, 16 Dec',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kDarkBlue, fontSize: 14)),
+                                  Text('Sick',
+                                      style: kPoppinsMediumBold.copyWith(
+                                          color: kLightBlue, fontSize: 14)),
+                                ],
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: BoxDecoration(
+                                    color: kLightWhite,
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: const InkWell(
+                                    child: Icon(
+                                  Icons.arrow_right,
+                                  color: kGrey,
+                                )),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-              Expanded(
-                  child: Container(
-                padding: const EdgeInsets.all(20),
-                width: MediaQuery.of(context).size.width,
-                height: double.infinity,
-                color: const Color(0xffecedf2),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 120,
-                        margin: const EdgeInsets.symmetric(vertical: 5.0),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Container(
-                              height: 50,
-                              width: 50,
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(12.0),
-                                  child: Image.asset('assets/icons/user.png')),
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Container(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  AutoSizeText(
-                                    'half day application',
-                                    style: GoogleFonts.poppins(
-                                      textStyle: const TextStyle(
-                                          fontSize: 18,
-                                          color: Color(0xff1f1f1f)),
-                                    ),
-                                    maxLines: 3,
-                                  ),
-                                  const SizedBox(
-                                    height: 4,
-                                  ),
-                                  // Row(
-                                  //   mainAxisAlignment:
-                                  //       MainAxisAlignment
-                                  //           .start,
-                                  //   children: [
-                                  //     Row(
-                                  //       children: [
-                                  //         Text(
-                                  //           '${_santriListModel
-                                  //                   .santri[itemIndex]
-                                  //                   .nis}',
-                                  //           style: GoogleFonts
-                                  //               .poppins(
-                                  //             textStyle: const TextStyle(
-                                  //                 fontSize:
-                                  //                     14,
-                                  //                 color:
-                                  //                     Color(0xff2b2b2b)),
-                                  //           ),
-                                  //         )
-                                  //       ],
-                                  //     )
-                                  //   ],
-                                  // )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-            ],
-          ),
+            )),
+          ],
         ),
       ),
     );
